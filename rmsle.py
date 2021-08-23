9c620dc65788c67bec64934fbad1d62d0aa7c59c
Vinit <20949380+vinit0108@users.noreply.github.com>
2021-08-23 17:29:14 +0530

    Add files via upload

---
+++ rmsle.py
@@ -0,0 +1,9 @@
+def rmsle(y, y_,convertExp=True):
+    if convertExp:
+        y = np.exp(y),
+        y_ = np.exp(y_)
+    log1 = np.nan_to_num(np.array([np.log(v + 1) for v in y]))
+    log2 = np.nan_to_num(np.array([np.log(v + 1) for v in y_]))
+    calc = (log1 - log2) ** 2
+    return np.sqrt(np.mean(calc))
+
